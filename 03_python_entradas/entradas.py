bee25bf32cc8aadbb692a6facb453edd98646f1b
GuilhermeFaveron <guilhermefaveron@gmail.com>
2026-05-09 10:40:50 -0300

    Python Entradas

---
+++ 03_python_entradas/entradas.py
@@ -0,0 +1,10 @@
+#autor : Guilherme Faveron
+#Projeto: estudando entradas e saidas em python
+
+#Declaração de variáveis
+valor1 = 10
+valor2 = 20
+resultado = valor1 + valor2
+
+#Exibindo o resultado
+print(resultado)
---
+++ 03_python_entradas/entradas4.py
@@ -0,0 +1,22 @@
+#autor: Guilherme Faveron
+#Projeto: entrafdas com operações
+
+#variaveis
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor:'))
+resultado1 = valor1 + valor2
+resultado2 = valor1 - valor2
+resultado3 = valor1 * valor2
+resultado4 = valor1 / valor2
+resultado5 = valor1 // valor2
+resultado6 = valor1 % valor2
+resultado7 = valor1 ** valor2
+
+#exibindo os resultados
+print('O resultado da soma é: ', resultado1)
+print('O resultado da subtração é: ', resultado2)
+print('O resultado da multiplicação é: ', resultado3)
+print('O resultado da divisão é: ', resultado4)
+print('O resultado da divisão inteira é: ', resultado5)
+print('O resultado do módulo é: ', resultado6)
+print('O resultado da potência é: ', resultado7)
