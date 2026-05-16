98e851307ef97c3e03cb7311ea2a48771dda983a
GuilhermeFaveron <guilhermefaveron@gmail.com>
2026-05-16 20:31:20 -0300

    Update atividade01_ifelse.py

--- 04-python_condicional/atividade01_ifelse.py
+++ 04-python_condicional/atividade01_ifelse.py
@@ -3,7 +3,7 @@
 
 #Criação das variaveis
 nome = input('Digite seu nome: ')
-nota = float(input('Digte a nota final: ')) 
+nota = float(input('Digite a nota final: ')) 
 if nota>=6:
     print('Aluno aprovado')
 else:
